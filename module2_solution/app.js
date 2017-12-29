419b600d21f6166670e83820c2eb393abd8326a0
Lorenzo Martínez Moret <lorenzo.martinez@gmail.com>
2017-12-29 19:35:20 +0100

    Service implemented

--- module2_solution/app.js
+++ module2_solution/app.js
@@ -22,7 +22,32 @@ AlreadyBoughtController = function (ShoppingListCheckOffService) {
 }
 
 ShoppingListCheckOffService = function () {
-  var service = this;
+  let service = this;
+
+  let toBuyItems = [
+    { name: "cookies", quantity: 10 },
+    { name: "orange juice", quantity: 5 },
+    { name: "milk", quantity: 1 },
+    { name: "coffee", quantity: 2 },
+    { name: "soda", quantity: 7 }
+  ];
+
+  let boughtItems = [];
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  }
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  }
+
+  service.buyItem = function (itemIndex) {
+    let item = toBuyItems[itemIndex];
+    toBuyItems.splice(itemIndex, 1);
+    boughtItems.push(item);
+  }
+
 }
 
 })();
