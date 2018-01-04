6b7c4c3296f938e2dc70101f0d81ea849993905d
Lorenzo Martínez Moret <lorenzo.martinez@gmail.com>
2018-01-04 20:46:20 +0100

    Added message

--- module3_solution/app.js
+++ module3_solution/app.js
@@ -13,15 +13,21 @@ function NarrowItDownController(MenuSearchService) {
   let menu = this;
 
   menu.searchTerm = "";
+  menu.message = "";
   menu.found = [];
+
   menu.showMatchedMenuItems = function () {
     let promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+    menu.message = "";
 
     promise.then(function (response) {
       menu.found = response;
+      if (menu.found.length === 0) {
+        menu.message = "Nothing Found!";
+      }
     })
     .catch(function (error) {
-      console.log("There was a problem while fetching the menu items: ", error);
+      menu.message =  "There was a problem while fetching the menu items: " + error;
       menu.found = [];
     })
   };
@@ -47,10 +53,12 @@ function MenuSearchService($http, MenuServiceBasePath) {
     }).then(function (result) {
       let foundItems = [];
 
-      for(let i=0; i < result.data.menu_items.length; i++){
-        let item = result.data.menu_items[i];
-        if (item.description.toLowerCase().indexOf(searchTerm) !== -1) {
-          foundItems.push(item);
+      if (searchTerm !== ""){
+        for(let i=0; i < result.data.menu_items.length; i++){
+          let item = result.data.menu_items[i];
+          if (item.description.toLowerCase().indexOf(searchTerm) !== -1) {
+            foundItems.push(item);
+          }
         }
       }
 
@@ -68,8 +76,9 @@ function FoundItems() {
       foundItems: "<",
       onRemove: "&"
     },
+    transclude: true,
     controllerAs: "menu",
-    bindToCotroller: true 
+    bindToCotroller: true
   };
 
   return ddo;
