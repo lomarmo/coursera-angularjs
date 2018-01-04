a38a179d311069b929a36888d03c626fc11e2b91
Lorenzo Martínez Moret <lorenzo.martinez@gmail.com>
2018-01-04 13:15:14 +0100

    Solution implemented

---
+++ module3_solution/app.js
@@ -0,0 +1,78 @@
+(function () {
+'use strict';
+
+angular.module("NarrowItDownApp", [])
+
+.controller("NarrowItDownController", NarrowItDownController)
+.service("MenuSearchService", MenuSearchService)
+.directive("foundItems", FoundItems)
+.constant("MenuServiceBasePath", "https://davids-restaurant.herokuapp.com");
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  let menu = this;
+
+  menu.searchTerm = "";
+  menu.found = [];
+  menu.showMatchedMenuItems = function () {
+    let promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+
+    promise.then(function (response) {
+      menu.found = response;
+    })
+    .catch(function (error) {
+      console.log("There was a problem while fetching the menu items: ", error);
+      menu.found = [];
+    })
+  };
+
+  menu.removeItem = function (itemIndex) {
+    menu.found.splice(itemIndex, 1);
+  }
+
+}
+
+MenuSearchService.$inject = ['$http', 'MenuServiceBasePath'];
+function MenuSearchService($http, MenuServiceBasePath) {
+
+  let service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    let serviceEndPoint = MenuServiceBasePath + "/menu_items.json";
+    searchTerm = searchTerm.toLowerCase();
+
+    return $http({
+      method: "GET",
+      url: serviceEndPoint
+    }).then(function (result) {
+      let foundItems = [];
+
+      for(let i=0; i < result.data.menu_items.length; i++){
+        let item = result.data.menu_items[i];
+        if (item.description.toLowerCase().indexOf(searchTerm) !== -1) {
+          foundItems.push(item);
+        }
+      }
+
+      return foundItems;
+    })
+  }
+
+}
+
+function FoundItems() {
+  let ddo = {
+    restrict: "E",
+    templateUrl: "foundItems.html",
+    scope: {
+      foundItems: "<",
+      onRemove: "&"
+    },
+    controllerAs: "menu",
+    bindToCotroller: true 
+  };
+
+  return ddo;
+}
+
+})();
